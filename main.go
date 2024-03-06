b77788c1b5b0ef05cc063d9ace0a44bf229d24e4
Andy Dowell <andrewdowell_84@hotmail.com>
2024-03-06 15:14:16 +0000

    with conn test code (wip)

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 func main() {
+
 	ctx := context.Background()
 
 	headers := metadata.Pairs(
@@ -19,6 +20,7 @@ func main() {
 		"developer-token", os.Getenv("DEVELOPER_TOKEN"),
 		"login-customer-id", os.Getenv("CUSTOMER_ID"),
 	)
+
 	ctx = metadata.NewOutgoingContext(ctx, headers)
 
 	cred := grpc.WithTransportCredentials(credentials.NewClientTLSFromCert(nil, ""))
@@ -28,37 +30,61 @@ func main() {
 		panic(err)
 	}
 
+	fmt.Println(conn.GetState())
 	defer conn.Close()
+
+	customerServiceClient := services.NewCustomerServiceClient(conn)
+
+	fmt.Println(conn.GetState())
+
+	accessibleCustomers, err := customerServiceClient.ListAccessibleCustomers(ctx, &services.ListAccessibleCustomersRequest{})
+
+	fmt.Println(conn.GetState())
+
+	fmt.Println(accessibleCustomers.String())
+
+	if err != nil {
+		panic(err)
+	}
+
+	/*accessibleCustomers, err := customerServiceClient.ListAccessibleCustomers(ctx, &services.ListAccessibleCustomersRequest{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	for _, customer := range accessibleCustomers.ResourceNames {
+	fmt.Println("ResourceName: " + customer)
+	}*/
 
 	glicd := "11ppppp"
 	customerId := "aaaaa"
-	jobId:= int32(68)
-	
+	jobId := int32(68)
+
 	clickConversion := services.ClickConversion{
 		Gclid: &glicd,
 	}
 
-	conversions:= []*services.ClickConversion{&clickConversion}
+	conversions := []*services.ClickConversion{&clickConversion}
 
-	uploadCallConversionsRequest :=  services.UploadClickConversionsRequest{
-		CustomerId: customerId,
-		Conversions: conversions,
-		PartialFailure: true,
-		ValidateOnly: false,
-		DebugEnabled: true,
-		JobId: &jobId,
+	uploadCallConversionsRequest := services.UploadClickConversionsRequest{
+		CustomerId:     customerId,
+		Conversions:    conversions,
+		PartialFailure: false,
+		ValidateOnly:   false,
+		DebugEnabled:   false,
+		JobId:          &jobId,
 	}
 
 	conversionUploadServiceClient := services.NewConversionUploadServiceClient(conn)
-	
-	resp,err := conversionUploadServiceClient.UploadClickConversions(
+
+	resp, err := conversionUploadServiceClient.UploadClickConversions(
 		ctx,
 		&uploadCallConversionsRequest,
-
 	)
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Println(resp)
-	fmt.Println(err)
 }
-
-	
